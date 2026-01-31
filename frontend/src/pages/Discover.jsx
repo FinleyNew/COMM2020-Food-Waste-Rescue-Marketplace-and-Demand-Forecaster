d64012871f8140f7332b4e4e45b4ef95f30807a6
TomLambe23 <tl674@exeter.ac.uk>
2026-01-31 15:37:07 +0000

    Discover Page Development

--- frontend/src/pages/Discover.jsx
+++ frontend/src/pages/Discover.jsx
@@ -1,13 +1,26 @@
 import { Routes, Route, Link } from "react-router-dom";
+import './Discover.css'
 function Discover() {
   return (
     <>
-    
-      <nav>
-        <Link to="/login">Login Page</Link> |{" "}
-        <Link to="/streaks">Streaks</Link> |{" "}
-        <Link to="/codes">Codes</Link>
+      <nav class="row">
+        <Link to="/login" className="button"><b>Login Page</b></Link>
+        <Link to="/streaks" className="button"><b>Streaks</b></Link>
+        <Link to="/codes" className="button"><b>Codes</b></Link>
       </nav>
+      <h1 className="headline">Bundles</h1>
+      <section>
+        <div className="text-block">
+          <p className="desc">Food Item</p>
+          <p>Food Item</p>
+        </div>
+        <div className="formatter">
+          <a target="_blank" rel="noopener noreferrer" href="https://media.istockphoto.com/id/1457433817/photo/group-of-healthy-food-for-flexitarian-diet.jpg?s=612x612&w=0&k=20&c=v48RE0ZNWpMZOlSp13KdF1yFDmidorO2pZTu2Idmd3M=">
+          </a>
+          <img src="https://media.istockphoto.com/id/1457433817/photo/group-of-healthy-food-for-flexitarian-diet.jpg?s=612x612&w=0&k=20&c=v48RE0ZNWpMZOlSp13KdF1yFDmidorO2pZTu2Idmd3M=" alt="Food" className="thumbnail"/>
+            <p>Company Name</p>
+        </div>
+      </section>
     </>
   );
 }
