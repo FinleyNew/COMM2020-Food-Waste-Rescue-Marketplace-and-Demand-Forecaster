12d47666d21534c23c4574ff8cd8ab40f8adfec7
FinleyNew <fjfn201@exeter.ac.uk>
2026-02-03 11:04:31 +0000

    Started consumers endpoint

--- backend/app/api/v1/endpoints/consumers.py
+++ backend/app/api/v1/endpoints/consumers.py
@@ -1,3 +1,11 @@
+from fastapi import APIRouter, Depends, HTTPexception
+from sqlmodel import Session
+from app.api.deps import ConsumerDep, SessionDep
+from app.schemas.consumer import ConsumerPublic
+from app.services import user as user_service
+from app.services import consumer as consumer_service
+
+router = APIRouter()
 
 @router.get("/me", response_model = ConsumerPublic)
-def get_current_consumer(current_consumer: ConsumerDep)
+def get_current_consumer(current_consumer: ConsumerDep, db: SessionDep):
