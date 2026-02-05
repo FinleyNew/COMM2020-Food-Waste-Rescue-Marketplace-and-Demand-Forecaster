de2aa2b0bc00eaa12a067fd7341d243b1f268854
JamesHickson <jmh280@exeter.ac.uk>
2026-02-05 16:05:20 +0000

    Add User model with relationships

---
+++ backend/app/models/user.py
@@ -0,0 +1,14 @@
+from typing import Optional, List, TYPE_CHECKING
+from sqlmodel import Field, SQLModel, Relationship
+
+if TYPE_CHECKING:
+    from .seller import Seller
+    from .consumer import Consumer
+
+
+class User(SQLModel, table=True):
+    user_id: Optional[int] = Field(default=None, primary_key=True)
+    role: str
+
+    seller: Optional["Seller"] = Relationship(back_populates="user")
+    consumer: Optional["Consumer"] = Relationship(back_populates="user")
