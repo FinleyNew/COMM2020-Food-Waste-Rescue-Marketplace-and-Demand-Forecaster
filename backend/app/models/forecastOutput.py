84330ca1c4d6ee9084c651a6b493d6047ddb1bd7
FinleyNew <fjfn201@exeter.ac.uk>
2026-02-06 16:59:55 +0000

    Changed ForecastOutput to Forecast

--- backend/app/models/forecastOutput.py
+++ backend/app/models/forecastOutput.py
@@ -5,8 +5,8 @@ if TYPE_CHECKING:
     from .seller import Seller
     from .bundlePosting import BundlePosting
 
-class ForecastOutput(SQLModel, table=True):
-    output_id: Optional[int] = Field(default=None, primary_key=True)
+class Forecast(SQLModel, table=True):
+    forecast_id: Optional[int] = Field(default=None, primary_key=True)
     user_id: Optional[int] = Field(default=None, foreign_key="user.user_id")
     posting_id: Optional[int] = Field(default=None, foreign_key="bundleposting.posting_id")
     predicted_reservations: float
