684e01e26b800e8d4e1656ab376187cb9a85724a
harrylewis200 <hl870@exeter.ac.uk>
2026-02-08 17:16:23 +0000

    add-bundles page

--- frontend/src/sellerPages/AddBundles.jsx
+++ frontend/src/sellerPages/AddBundles.jsx
@@ -3,41 +3,73 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 function AddBundles() {
-  let bundleName = "";
-  let location = "";
-  let collectionTime = "";
+  const [bundleName, setBundleName] = useState("");
+  const [location, setLocation] = useState("");
+  const [collectionTime, setCollectionTime] = useState("");
+
   function addBundle(){
-    //code for exporting to json and sending to backend
+    const data = {
+      bundleName: bundleName,
+      location: location,
+      collectionTime: collectionTime
+    };
+
+    fetch("http://localhost:5000/api/add-bundles", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    });
   }
+
   return (
     <>
-
-      <nav class="row">
+      <nav className="row">
         <Link to="/login" className="button"><b>Login Page</b></Link>
         <Link to="/current-bundles" className="button"><b>Current Bundles</b></Link>
         <Link to="/analytics" className="button"><b>Analytics</b></Link>
       </nav>
+
       <h1 className="headline">Add Bundles</h1>
+
       <section>
         <div className="textBlock">
-            <div className="addBundle">
-              <label for="bundle_name">Enter Bundle Name : </label>
-              <input type = "text" id="bundle_name"></input>
-              <br></br>
-              <label for="bundle_location">Enter Location :          </label>
-              <input type = "text" id="bundle_location"></input>
-              <br></br>
-              <label for="bundle_collectionTime">Enter Bundle Name : </label>
-              <input type = "text" id="bundle_collectionTime"></input>
-            </div>
-            
-            <button onClick={addBundle}>Login</button>
+
+          <label htmlFor="bundle_name">Enter Bundle Name :</label>
+          <input
+            id="bundle_name"
+            type="text"
+            value={bundleName}
+            onChange={(e) => setBundleName(e.target.value)}
+          />
+
+          <br />
+
+          <label htmlFor="location">Enter Location :</label>
+          <input
+            id="location"
+            type="text"
+            value={location}
+            onChange={(e) => setLocation(e.target.value)}
+          />
+
+          <br />
+
+          <label htmlFor="collectionTime">Enter Collection Time :</label>
+          <input
+            id="collectionTime"
+            type="text"
+            value={collectionTime}
+            onChange={(e) => setCollectionTime(e.target.value)}
+          />
+
+          <button onClick={addBundle}>Add Bundle</button>
+
         </div>
       </section>
-      
-      
     </>
   );
 }
 
-export default AddBundles
+export default AddBundles;
