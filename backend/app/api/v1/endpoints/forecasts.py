9431fc228eee73c79667055257c827ca77581987
FinleyNew <fjfn201@exeter.ac.uk>
2026-02-08 10:19:55 +0000

    Set up alembic

--- backend/app/api/v1/endpoints/forecasts.py
+++ backend/app/api/v1/endpoints/forecasts.py
@@ -4,6 +4,6 @@ from app.schemas.forecast import ForecastPublic
 
 router = APIRouter()
 
-@router.get("me", response_model= ForecastPublic)
+@router.get("/me", response_model= ForecastPublic)
 def get_current_sellers_forecasts(current_seller: SellerDep, db: SessionDep):
     return current_seller.forecasts
