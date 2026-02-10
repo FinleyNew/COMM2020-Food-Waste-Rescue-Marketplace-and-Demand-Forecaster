85d092699501c0dae9f1dde311c9219ab193a34d
FinleyNew <fjfn201@exeter.ac.uk>
2026-02-10 09:13:39 +0000

    Changed consumer endpoint

--- backend/app/api/v1/endpoints/consumers.py
+++ backend/app/api/v1/endpoints/consumers.py
@@ -8,6 +8,7 @@ router = APIRouter()
 
 @router.get("/me", response_model = ConsumerPublic)
 def get_current_consumer(current_consumer: ConsumerDep, db: SessionDep):
-    if current_consumer.streak > 0:
-        consumer_service.check_streak(consumer_id=current_consumer.user_id, db=db)
+    consumer_id = current_consumer.user_id
+    if current_consumer.streak > 0 and consumer_id:
+        consumer_service.check_streak(consumer_id=consumer_id, db=db)
     return current_consumer
