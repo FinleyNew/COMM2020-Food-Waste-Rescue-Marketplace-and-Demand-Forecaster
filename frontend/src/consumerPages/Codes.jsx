808c935b892961557965dbc1d15e77a034857307
harrylewis200 <hl870@exeter.ac.uk>
2026-02-11 11:13:20 +0000

    current reservations working (codes page)

--- frontend/src/consumerPages/Codes.jsx
+++ frontend/src/consumerPages/Codes.jsx
@@ -1,6 +1,28 @@
 import { Routes, Route, Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 import './Discover.css'
 function Codes() {
+  const [bundles, setBundles] = useState([]); //create state
+    
+    useEffect(() => {
+      const token = localStorage.getItem('token');
+      fetch("http://127.0.0.1:8000/api/v1/reservations/me",{
+        headers:{
+          "Authorization": `Bearer ${token}`,
+          "Content-Type": "application/json"
+        }
+      }) //fetch here , useeffect means it only fetches once
+        .then(res => res.json())
+        .then(data => {
+          console.log("API DATA:", data); 
+          setBundles(data);
+        })
+        .catch(err => {
+          console.error("Error fetching bundles:", err);
+          alert("No data ");
+        });
+    }, []);
+    //if (!bundle) return <p>Loading bundle...</p>;
   let companyName = "Amazon";
   let code = "example code";
   let bundleName = "idk";
@@ -15,21 +37,31 @@ function Codes() {
       </nav>
       <h1 className="headline">Codes</h1>
       <section>
+        {bundles.map(bundle => (
+          <div key={bundle.posting_id}>
+              <section>
           <div className="textBlock">
             <img src="https://media.istockphoto.com/id/1457433817/photo/group-of-healthy-food-for-flexitarian-diet.jpg?s=612x612&w=0&k=20&c=v48RE0ZNWpMZOlSp13KdF1yFDmidorO2pZTu2Idmd3M=" alt="Food" className="leftImg"/>
             <div className="desc">
-              <p className="desc">{companyName}</p>
-              <p className="desc">Bundle Name - {bundleName}</p>
+              <p className="desc">Reservation ID : {bundle.reservation_id}</p>
+              <p className="desc">Claim Code - {bundle.claim_code}</p>
             </div>
           </div>
           <div className="textBlock">
             <div className="desc">
-              <p className="codeText">Code - {code}</p>
-              <p>Location - {location}</p>
-              <p>Collection Time - {collectionTime}</p>
+              <p className="codeText">Status - {bundle.status}</p>
+              
+              <p>Collection Time - {bundle.timestamp}</p>
             </div>
           </div>
           </section>
+          </div>
+        ))}
+
+
+
+      </section>
+      
       </>
     );
 }
