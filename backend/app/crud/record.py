94b84ff967836425fdccf2a4ce3c493e2c706f1c
FinleyNew <fjfn201@exeter.ac.uk>
2026-02-11 17:12:39 +0000

    Finished get records for forecast function

--- backend/app/crud/record.py
+++ backend/app/crud/record.py
@@ -1,9 +1,16 @@
 from sqlmodel import Session, select, col, func, extract
 from typing import Sequence
 from datetime import datetime
+from app.models.enums import Category
 from app.models import Record
 
-def get_records_for_forecast(seller_id: int, db: Session) -> Sequence[Record]:
+def get_records_for_forecast(seller_id: int, category: Category, raining: bool, db: Session) -> Sequence[Record]:
     current_dow = (datetime.now().weekday() + 1) % 7
-    statement = select(Record).where(Record.user_id == seller_id).where(extract('dow', func.lower(Record.pickup_window)) == current_dow)
+    statement = (
+        select(Record)
+        .where(Record.user_id == seller_id)
+        .where(extract('dow', func.lower(Record.pickup_window)) == current_dow)
+        .where(Record.category == category)
+        .where(Record.raining == raining)
+    )
     return db.exec(statement).all()
