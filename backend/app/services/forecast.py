062c33561ce4f70ddf470214b84f19597c9ea7a7
FinleyNew <fjfn201@exeter.ac.uk>
2026-02-11 17:24:23 +0000

    Started create forecast service

--- backend/app/services/forecast.py
+++ backend/app/services/forecast.py
@@ -1,6 +1,8 @@
+from sqlmodel import Session
+from app.schemas.bundlePosting import BundlePostingCreate
 
 
-def create_forecast(owner_id: int):
+def create_forecast(bundle_in: BundlePostingCreate, owner_id: int, db: Session):
     #Get any data you need from records
     #Process that data into something usable
     #Call the create_forecast crud function to actually add it to the database
