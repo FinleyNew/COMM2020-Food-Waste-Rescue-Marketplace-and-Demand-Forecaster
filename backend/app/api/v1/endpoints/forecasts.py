6e27adec365416649f3dcc67ccdef030d0ae4867
FinleyNew <fjfn201@exeter.ac.uk>
2026-02-12 12:51:25 +0000

    fixed get records endpoint

--- backend/app/api/v1/endpoints/forecasts.py
+++ backend/app/api/v1/endpoints/forecasts.py
@@ -1,9 +1,13 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from app.api.deps import SellerDep, SessionDep
 from app.schemas.forecast import ForecastPublic
+from typing import List
 
 router = APIRouter()
 
-@router.get("/me", response_model= ForecastPublic)
+@router.get("/me", response_model= List[ForecastPublic])
 def get_current_sellers_forecasts(current_seller: SellerDep, db: SessionDep):
-    return current_seller.forecasts
+    forecasts = current_seller.forecasts
+    if not forecasts:
+        raise HTTPException(status_code = 404, detail = "No forecasts found")
+    return forecasts
--- backend/app/api/v1/endpoints/records.py
+++ backend/app/api/v1/endpoints/records.py
@@ -1,9 +1,13 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from app.api.deps import SellerDep, SessionDep
 from app.schemas.record import RecordPublic
+from typing import List
 
 router = APIRouter()
 
-@router.get("/me", response_model= RecordPublic)
+@router.get("/me", response_model= List[RecordPublic])
 def get_current_sellers_records(current_seller: SellerDep, db: SessionDep):
-    return current_seller.records
+    records = current_seller.records
+    if not records:
+        raise HTTPException(status_code = 404, detail = "No records found")
+    return records
