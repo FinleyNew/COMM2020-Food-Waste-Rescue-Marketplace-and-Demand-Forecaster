c90bbeaee6962811f901cfffcd6aa85e81402cf5
FinleyNew <fjfn201@exeter.ac.uk>
2026-02-13 11:40:48 +0000

    Finished commenting api folder

--- backend/app/api/deps.py
+++ backend/app/api/deps.py
@@ -14,21 +14,25 @@ from app.models.seller import Seller
 from app.models.consumer import Consumer
 from app.schemas.token import TokenPayload
 
-
 def get_db() -> Generator:
-    # Open a new "session" (a single conversation with the DB)
+    # Open a new session
     with Session(engine) as session:
         yield session
-    # Once the request is finished, the session automatically closes here.
-    
+    # As it is a generator the session automatically closes here
+
+# This is the code we'll need when implementing the login system
 # reusable_oauth2 = OAuth2PasswordBearer(
 #     tokenUrl=f"{settings.API_STR}/login/access-token"
 # )
 
+#Currently using thei as Tokens are hardcoded
 reusable_oauth2 = HTTPBearer()
 
+# This can be called in an endpoint and will get a session to be used for the
+# duration of that endpoint
 SessionDep = Annotated[Session, Depends(get_db)]
 
+# This function is used to decrypt the token into a user_id and then get that user from the DB
 def get_current_user(db: SessionDep, token_obj: HTTPAuthorizationCredentials = Depends(reusable_oauth2)) -> User:
     token = token_obj.credentials
     try:
@@ -48,8 +52,12 @@ def get_current_user(db: SessionDep, token_obj: HTTPAuthorizationCredentials = D
 
     return user
 
+# This can be called to get the current user using the token
 CurrentUser = Annotated[User, Depends(get_current_user)]
 
+# This function is used to get the current seller
+# It uses the CurrentUser and then checks if their role is a seller or not
+# If it is it gets that seller from the DB
 def get_current_seller(db: SessionDep, current_user: CurrentUser) -> Seller:
     if current_user.role != "seller":
         raise HTTPException(status_code=400, detail="This user is not a seller")
@@ -60,6 +68,9 @@ def get_current_seller(db: SessionDep, current_user: CurrentUser) -> Seller:
     
     return seller
 
+# This function is used to get the current consumer
+# It uses the CurrentUser and then checks if their role is a consumer or not
+# If it is it gets that consumer from the DB
 def get_current_consumer(db: SessionDep, current_user: CurrentUser) -> Consumer:
     if current_user.role != "consumer":
         raise HTTPException(status_code=400, detail="This user is not a consumer")
@@ -73,6 +84,8 @@ def get_current_consumer(db: SessionDep, current_user: CurrentUser) -> Consumer:
     
     return consumer
 
+# This function is used to get the current admin
+# Currently not in use
 def get_current_admin(current_user: CurrentUser) -> User:
     if current_user.role != "admin":
         raise HTTPException(status_code=400, detail="This user is not an admin")
