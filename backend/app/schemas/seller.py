9654f64e1a672bfbe54e187f40a1feb3c899051c
FinleyNew <fjfn201@exeter.ac.uk>
2026-02-13 13:56:47 +0000

    Finished commenting schemas

--- backend/app/schemas/seller.py
+++ backend/app/schemas/seller.py
@@ -1,9 +1,13 @@
 from sqlmodel import SQLModel
 
+# The base schema for sellers
 class SellerBase(SQLModel):
     name: str
     location: str
     open_hours: str
 
+#No create schema as we cannot create users yet
+
+# The public schema for seller
 class SellerPublic(SellerBase):
     user_id: int
--- backend/app/schemas/token.py
+++ backend/app/schemas/token.py
@@ -1,8 +1,11 @@
 from pydantic import BaseModel
 
+# The schema for sending tokens
+# Currently not in use
 class Token(BaseModel):
     access_token: str
     token_type: str = "bearer"
 
+# The schema for recieving/validating tokens
 class TokenPayload(BaseModel):
     sub: str | None = None
