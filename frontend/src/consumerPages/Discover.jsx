1c575a70145a909703ac584324d838d7ca1400e0
TomLambe23 <tl674@exeter.ac.uk>
2026-02-13 12:17:44 +0000

    Ordered Discover Bundles

--- frontend/src/consumerPages/Discover.jsx
+++ frontend/src/consumerPages/Discover.jsx
@@ -44,12 +44,12 @@ function Discover() {
       </nav>
       <h1 className="headline">Bundles</h1>
       
-      <section>
+      <section className="column">
             {bundles.map(bundle => (
               <div key={bundle.posting_id}>
                 <Link to={`/bundle/${bundle.posting_id}`} className="sectionPage">
 
-                  <section>
+                  <section className="bundleEntry">
                     <div className="textBlock">
                       <img src="https://media.istockphoto.com/id/1457433817/photo/group-of-healthy-food-for-flexitarian-diet.jpg?s=612x612&w=0&k=20&c=v48RE0ZNWpMZOlSp13KdF1yFDmidorO2pZTu2Idmd3M=" alt="Food" className="leftImg"/>
                       <div className="desc">
