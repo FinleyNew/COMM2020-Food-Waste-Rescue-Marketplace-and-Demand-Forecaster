a0f25f189bc9f236d23e89d940fad59e6e22cbe7
TomLambe23 <tl674@exeter.ac.uk>
2026-02-16 16:21:33 +0000

    Codes Page Formatting

--- frontend/src/consumerPages/Codes.jsx
+++ frontend/src/consumerPages/Codes.jsx
@@ -35,27 +35,27 @@ function Codes() {
       <section className="column">
         {bundles.map(bundle => (
           <div key={bundle.posting_id}>
-              <section>
-          <div className="textBlock">
-            <img src="https://media.istockphoto.com/id/1457433817/photo/group-of-healthy-food-for-flexitarian-diet.jpg?s=612x612&w=0&k=20&c=v48RE0ZNWpMZOlSp13KdF1yFDmidorO2pZTu2Idmd3M=" alt="Food" className="leftImg"/>
-            <div className="desc">
-              <p className="desc">Reservation ID : {bundle.reservation_id}</p>
-              <p className="desc">Claim Code - {bundle.claim_code}</p>
+          <section>
+            <div className="bundleEntry">
+              <div className="textBlock">
+                <img src="https://media.istockphoto.com/id/1457433817/photo/group-of-healthy-food-for-flexitarian-diet.jpg?s=612x612&w=0&k=20&c=v48RE0ZNWpMZOlSp13KdF1yFDmidorO2pZTu2Idmd3M=" alt="Food" className="leftImg"/>
+                <div className="desc">
+                  <p className="desc">Reservation ID : {bundle.reservation_id}</p>
+                  <p className="desc">Claim Code - {bundle.claim_code}</p>
+                </div>
+              </div>
+              <div className="textBlock">
+                <div className="desc">
+                  <p className="codeText">Status - {bundle.status}</p>
+                  
+                  <p>Collection Time - {bundle.timestamp}</p>
+                </div>
+              </div>
+              </div>
+            </section>
+            <div className="whitespace" aria-hidden="true"></div>
             </div>
-          </div>
-          <div className="textBlock">
-            <div className="desc">
-              <p className="codeText">Status - {bundle.status}</p>
-              
-              <p>Collection Time - {bundle.timestamp}</p>
-            </div>
-          </div>
-          </section>
-          </div>
         ))}
-
-
-
       </section>
       
       </>
