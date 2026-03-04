30d942d033c18ceaa5a06b4b37c6b8d03157d600
FinleyNew <fjfn201@exeter.ac.uk>
2026-03-04 16:13:08 +0000

    Changed login endpoint

--- backend/app/api/v1/endpoints/login.py
+++ backend/app/api/v1/endpoints/login.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter, Depends, HTTPException
 from fastapi.security import OAuth2PasswordRequestForm
 from jose import jwt
 from app.services import user as user_service
-from app.core.security import ALGORITHM
+from app.core import security
 from app.core.config import settings
 from app.schemas.token import Token
 
@@ -14,13 +14,10 @@ router = APIRouter()
 
 @router.post("/access-token", response_model = Token)
 def access_token(form: OAuth2PasswordRequestForm = Depends()):
-    user = user_service.get_user(form.username)
-    if not user or not settings.pwd_context.verify(form.password, user.hashed_password):
+    user = user_service.get_user_by_username(form.username)
+    if not user or not security.verify_password(form.password, user.hashed_password):
         raise HTTPException(status_code=401, detail="Incorrect credentials")
     
-    token = jwt.encode(
-        {"sub": str(user.user_id), "exp": datetime.now(timezone.utc) + timedelta(hours=8)},
-        settings.SECRET_KEY, algorithm=ALGORITHM
-    )
+    token = security.create_access_token(user.user_id)
 
     return {"access_token": token, "token_type": "bearer"}
