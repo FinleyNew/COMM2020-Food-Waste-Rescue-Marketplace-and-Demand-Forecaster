35e8fd2b70210dfe60e9c59647bb42041ee76c3f
FinleyNew <fjfn201@exeter.ac.uk>
2026-03-04 16:10:51 +0000

    Added functions to security.py

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -1,5 +1,22 @@
+from datetime import datetime, timedelta, timezone
 
-# This would store any security functions such as password hashing
-# Currently only stores the algorithm used for decrypting tokens
+from jose import jwt
+from app.core.config import settings
+
+from passlib.context import CryptContext
+
 
 ALGORITHM = "HS256"
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
+
+def get_password_hash(password: str) -> str:
+    return pwd_context.hash(password)
+
+def create_access_token(subject: str, expires_delta: timedelta = timedelta(hours=8)):
+    expire = datetime.now(timezone.utc)
+    payload = {"sub": str(subject), "exp": expire}
+    return jwt.encode(payload, settings.SECRET_KEY, algorithm=ALGORITHM)
