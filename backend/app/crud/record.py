19361a3808341156e371fd06f9899ee4e3258b89
FinleyNew <fjfn201@exeter.ac.uk>
2026-03-11 18:59:47 +0000

    Moved record creation to crud function

--- backend/app/crud/record.py
+++ backend/app/crud/record.py
@@ -3,6 +3,32 @@ from typing import Sequence
 from datetime import datetime
 from app.models.enums import Category
 from app.models import Record
+from app.models.bundlePosting import BundlePosting
+from app.services.reservation import get_no_show
+from random import randint
+
+def create_record(bundle_posting: BundlePosting, posting_id: int, db: Session) -> Record:
+    record = Record.model_validate(
+        bundle_posting,
+        update={
+            "raining": is_raining(),
+            "observed_reservations": bundle_posting.reserved,
+            "observed_no_show": get_no_show(posting_id=posting_id, db=db),
+            "observed_expired": bundle_posting.available
+        }
+    )
+    db.add(record)
+    db.commit()
+    db.refresh(record)
+    return record
+
+# Will just have a 1 in 10 chance of rain for any record
+def is_raining():
+    rnd = randint(0,9)
+    if rnd == 0:
+        return True
+    else:
+        return False
 
 # Crud function for getting all the records from the database
 # Is used for training the model
--- backend/app/services/record.py
+++ backend/app/services/record.py
@@ -5,8 +5,6 @@ from app.models.record import Record
 from app.models.bundlePosting import BundlePosting
 from app.schemas.record import RecordCreate
 from app.crud import record as record_crud
-from app.services.reservation import get_no_show
-from random import randint
 
 # The service function for creating a record
 # Not currently in use
@@ -14,24 +12,4 @@ def create_record(bundle_posting: BundlePosting, db: Session) -> Record:
     posting_id = BundlePosting.posting_id
     if not posting_id:
         raise Exception("Posting does not exist")
-    record = Record.model_validate(
-        bundle_posting,
-        update={
-            "raining": is_raining(),
-            "observed_reservations": bundle_posting.reserved,
-            "observed_no_show": get_no_show(posting_id=posting_id, db=db),
-            "observed_expired": bundle_posting.available
-        }
-    )
-    db.add(record)
-    db.commit()
-    db.refresh(record)
-    return record
-
-# Will just have a 1 in 10 chance of rain for any record
-def is_raining():
-    rnd = randint(0,9)
-    if rnd == 0:
-        return True
-    else:
-        return False
+    return record_crud.create_record(bundle_posting=bundle_posting, posting_id=posting_id, db=db)
