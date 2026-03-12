018630fa71bd31bba8109e46b5aede98fefb4a14
FinleyNew <fjfn201@exeter.ac.uk>
2026-03-12 14:22:00 +0000

    fixed get current sellers records and forecasts

--- backend/app/api/v1/endpoints/records.py
+++ backend/app/api/v1/endpoints/records.py
@@ -8,7 +8,4 @@ router = APIRouter()
 # Endpoint for getting the current sellers records
 @router.get("/me", response_model= List[RecordPublic])
 def get_current_sellers_records(current_seller: SellerDep, db: SessionDep):
-    records = current_seller.records
-    if not records:
-        raise HTTPException(status_code = 404, detail = "No records found")
-    return records
+    return current_seller.records or []
