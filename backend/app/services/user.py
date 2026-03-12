59b27a7c8cce1fc67f5f6d2a4872ed5c950cbb48
FinleyNew <fjfn201@exeter.ac.uk>
2026-03-12 16:29:30 +0000

    Fixed get user by email

--- backend/app/services/user.py
+++ backend/app/services/user.py
@@ -1,12 +1,7 @@
-
-# def create_new_user():
-
-# def authenticate_user():
-
 from sqlmodel import Session
 from app.models.user import User
 from app.crud import user as user_crud
 
 
-def get_user_by_email(email: str, db: Session) -> User:
+def get_user_by_email(email: str, db: Session) -> User | None:
     return user_crud.get_user_by_email(email=email, db=db)
