1f11f8bff5f2627e60c31c995ece4dc32db0baff
FinleyNew <fjfn201@exeter.ac.uk>
2026-03-17 17:23:48 +0000

    Implemented weather API to make records accurate

--- backend/app/crud/record.py
+++ backend/app/crud/record.py
@@ -1,10 +1,14 @@
+import httpx
 from sqlmodel import Session, select, col, func, extract, Time
 from typing import Sequence
 from datetime import datetime
 from app.models.enums import Category
 from app.models import Record
 from app.schemas.record import RecordAdminUpdate
-
+from app.models.bundlePosting import BundlePosting
+from app.core.config import settings
+from app.models.reservation import Reservation
+from app.crud import reservation as reservation_crud
 
 def update_record(db_record: Record, record_update: RecordAdminUpdate, pickup_window: str | None, db: Session) -> Record:
     update_data = record_update.model_dump(exclude_unset=True)
@@ -18,6 +22,24 @@ def update_record(db_record: Record, record_update: RecordAdminUpdate, pickup_wi
 def get_record_by_id(record_id: int, db: Session):
     statement = select(Record).where(Record.record_id == record_id)
     return db.exec(statement).one()
+
+def create_record(bundle_posting: BundlePosting, latitude: float | None, longitude: float | None, db: Session) -> Record:
+    posting_id = BundlePosting.posting_id
+    if not posting_id:
+        raise Exception("Posting does not exist")
+    record = Record.model_validate(
+        bundle_posting,
+        update={
+            "raining": is_raining(latitude=latitude, longitude=longitude),
+            "observed_reservations": bundle_posting.reserved,
+            "observed_no_show": get_no_show(posting_id=posting_id, db=db),
+            "observed_expired": bundle_posting.available
+        }
+    )
+    db.add(record)
+    db.commit()
+    db.refresh(record)
+    return record
 
 # Crud function for getting all the records from the database
 # Is used for training the model
@@ -45,3 +67,31 @@ def delete_record(record_id: int, db: Session):
     if record:
         db.delete(record)
         db.commit()
+
+# The service function for getting the number of no shows for a specific posting
+# Used when creating a record
+def get_no_show(posting_id: int, db: Session) -> int:
+    no_show_count = 0
+    reservations: Sequence[Reservation] = reservation_crud.get_reservations_by_posting(posting_id=posting_id, db=db)
+    for reservation in reservations:
+        if reservation.status == "":
+            no_show_count += 1
+    return no_show_count
+
+
+def is_raining(latitude: float | None, longitude: float | None) -> bool:
+    # If no coordinates are set assume no rain
+    if not latitude:
+        return False
+    with httpx.Client() as client:
+        response = client.get(
+            "https://api.openweathermap.org/data/2.5/weather",
+            params={
+                "lat": latitude,
+                "lon": longitude,
+                "appid": settings.OPENWEATHER_API_KEY
+            }
+        )
+        data = response.json()
+        weather_id = data["weather"][0]["id"]
+        return 500 <= weather_id <= 531
--- backend/app/services/record.py
+++ backend/app/services/record.py
@@ -6,7 +6,6 @@ from app.models.record import Record
 from app.models.bundlePosting import BundlePosting
 from app.schemas.record import RecordAdminUpdate, RecordCreate
 from app.crud import record as record_crud
-from app.services.reservation import get_no_show
 from random import randint
 
 def get_all_records(db: Session) -> Sequence[Record]:
@@ -26,30 +25,11 @@ def update_record(record_id: int, record_update: RecordAdminUpdate, db: Session)
 # The service function for creating a record
 # Not currently in use
 def create_record(bundle_posting: BundlePosting, db: Session) -> Record:
-    posting_id = BundlePosting.posting_id
-    if not posting_id:
-        raise Exception("Posting does not exist")
-    record = Record.model_validate(
-        bundle_posting,
-        update={
-            "raining": is_raining(),
-            "observed_reservations": bundle_posting.reserved,
-            "observed_no_show": get_no_show(posting_id=posting_id, db=db),
-            "observed_expired": bundle_posting.available
-        }
-    )
-    db.add(record)
-    db.commit()
-    db.refresh(record)
-    return record
+    latitude = bundle_posting.seller.latitude
+    longitude = bundle_posting.seller.longitude
+    return record_crud.create_record(bundle_posting=bundle_posting, latitude=latitude, longitude=longitude, db=db)
 
-# Will just have a 1 in 10 chance of rain for any record
-def is_raining():
-    rnd = randint(0,9)
-    if rnd == 0:
-        return True
-    else:
-        return False
+
     
 def delete_record(record_id: int, db: Session):
     record_crud.delete_record(record_id=record_id, db=db)
