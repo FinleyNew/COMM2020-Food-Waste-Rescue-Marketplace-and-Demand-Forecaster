4d56ade3613397f094ff265ea34fb9998010496b
FinleyNew <fjfn201@exeter.ac.uk>
2026-03-19 09:47:34 +0000

    Added badge service

---
+++ backend/app/services/badge.py
@@ -0,0 +1,69 @@
+
+
+def check_at_reservation():
+    check_good_start()
+
+def check_at_collection():
+    check_first_rescue()
+    check_on_a_roll()
+    check_locked_in()
+    check_relentless()
+    check_waste_warrior()
+    check_eco_advocate()
+    check_green_guardian()
+    check_punctual()
+    check_timekeeper()
+    check_unshakeable()
+    check_final_call()
+    check_weatherproof()
+    check_triple_threat()
+    check_familiar_face()
+    check_well_rounded()
+
+def check_good_start():
+    pass
+
+def check_first_rescue():
+    pass
+
+def check_on_a_roll():
+    pass
+
+def check_locked_in():
+    pass
+
+def check_relentless():
+    pass
+
+def check_waste_warrior():
+    pass
+
+def check_eco_advocate():
+    pass
+
+def check_green_guardian():
+    pass
+
+def check_punctual():
+    pass
+
+def check_timekeeper():
+    pass
+
+def check_unshakeable():
+    pass
+
+def check_final_call():
+    pass
+
+def check_weatherproof():
+    pass
+
+def check_triple_threat():
+    pass
+
+def check_familiar_face():
+    pass
+
+def check_well_rounded():
+    pass
