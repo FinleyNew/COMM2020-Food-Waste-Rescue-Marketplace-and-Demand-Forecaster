ed423a51f3698082fff5d02dac3ff6283f33c143
harrylewis200 <hl870@exeter.ac.uk>
2026-03-19 18:54:45 +0000

    update bundle done

--- frontend/src/sellerPages/CurrentBundles.jsx
+++ frontend/src/sellerPages/CurrentBundles.jsx
@@ -10,9 +10,29 @@ function CurrentBundles() {
   const [bundles, setBundles] = useState([]); //create state
   const [noBundles, setNoBundles] = useState(false);
   const [code, setCode] = useState("");
-
+  const [updateBundle, setUpdateBundle] = useState(false);
   const navigate = useNavigate();
   
+  const [bundleWeight, setBundleWeight] = useState("");
+  
+  const [bundlePrice,setBundlePrice] = useState("");
+  const [bundleAllergens,setBundleAllergens] = useState("");
+  const [bundleCategory, setBundleCategory] = useState("");
+  const [numberAvailable, setNumberAvailable] = useState("");
+  const [endTime, setEndTime] = useState("");
+  const [startTime, setStartTime] = useState("");
+  const token = localStorage.getItem('token');
+  const payload = JSON.parse(atob(token.split('.')[1]));
+  const today = new Date(); //need to get todays date to use to use the iso format
+  const dateString = today.toISOString().split("T")[0]; // YYYY-MM-DD, removing the time to replace with the selected time
+  const startDateTime = new Date(`${dateString}T${startTime}:00`); //creating new start time
+  const endDateTime = new Date(`${dateString}T${endTime}:00`); //creating new end time
+  const slots = Array.from({ length: 24 }, (_, i) => 
+    { const start = i;
+      const end = i + 1; 
+      return `${String(start).padStart(2,"0")}:00 - ${String(end).padStart(2,"0")}:00`;
+     });
+  const [data, setForecastData] = useState({});
 
 
   useEffect(() => {
@@ -41,22 +61,77 @@ function CurrentBundles() {
 
 
   const deleteBundle = (posting_id) => { //Function to delete bundles from backend
+    console.log(typeof(posting_id));
+    const token = localStorage.getItem('token');
+    console.log(token);
+    const postingID = posting_id;
     const API_URL = import.meta.env.VITE_API_URL;
     if (!window.confirm("Delete this bundle?")) return;
-
-    axios.get(`${API_URL}/api/v1/bundles/${posting_id}`, {
     
+    axios.delete(`${API_URL}/api/v1/bundles/${postingID}`, {
+      headers:{
+        Authorization: `Bearer ${token}`,
+      }
     })
     .then(response => {
       console.log("Deleted:", response.data);
       alert("Bundle deleted");
+      console.log(token);
     })
     .catch(err => { //Returns alert if an error occurs
-        console.error("Error fetching bundles:", err);
-        alert("No data ");
+         console.error("Backend error detail:", err.response?.data);  // <-- add this
+        console.error("Status:", err.response?.status);
     });
+    //window.location.reload();
     
     
+};
+
+const handleUpdateBundle = (posting_id) => {
+ 
+  setUpdateBundle(true);
+};
+
+
+const completeUpdateBundle = (posting_id) => {
+  const data = {
+      user_id: Number(payload.sub),
+      category: bundleCategory,
+      allergens: bundleAllergens,
+      available: Number(numberAvailable),
+      price: Number(bundlePrice),
+      weight: Number(bundleWeight),
+      start_time: startDateTime.toISOString(),
+      end_time: endDateTime.toISOString()
+    };
+    console.log(data);
+    //Function to prevent extreme entry for bundle weight with an alert
+    if(Number(bundleWeight)>10000 || Number(bundleWeight) <0){
+      alert("Weight must be positive and less than 10,000");
+      return;
+    }
+    //Alert for  extreme entry on number of available bundles
+    else if(Number(numberAvailable)>1000 || Number(numberAvailable)<0){
+      alert("Number available must be positive and less than 1000");
+      return;
+    }
+    //Check for bundle price being too great or negative, sending an alert if so
+    else if(Number(bundlePrice)>100 || Number(bundlePrice)<0){
+      alert("Price must be positive and less than 100");
+      return;
+    }
+    console.log(data);
+    
+    axios.patch(`${API_URL}/api/v1/bundles/${posting_id}`, data ,{
+      headers: {
+        "Authorization": `Bearer ${token}`,
+        "Content-Type": "application/json"
+      }
+    })
+      .catch(err => {
+          console.error("Request failed:", err);
+      }); 
+    window.location.reload();
 };
 
 const enterCode = (claim_code) => { //Function to return an entered code from the backend
@@ -146,6 +221,7 @@ const enterCode = (claim_code) => { //Function to return an entered code from th
                     <p>Category: {bundle.category}</p>
                     <p>Available: {bundle.available}</p>
                     <p>Weight: {bundle.weight}g</p>
+                    
                   </div>
                   {/* Holds the middle column data for the bundle entry */}
                   <div className="textBox">
@@ -164,12 +240,122 @@ const enterCode = (claim_code) => { //Function to return an entered code from th
                       />
                     </div>
                     {/* Button to delete bundles on click */}
+                    <button className="updateButton" onClick={() => handleUpdateBundle(bundle.posting_id)}>
+                        Update Bundle
+                    </button>
                     <button className="deleteButton" onClick={() => deleteBundle(bundle.posting_id)}>
                         Delete Bundle
                     </button>
                   </div>
                 </div>
-            </div>
+                {updateBundle && ( 
+                  <>
+                      <div className="row">
+                    {/* drop down for allergens */}
+                    <label htmlFor="category">Enter Bundle Category : </label>
+                    <select
+                      name="category"
+                      id="category"
+                      value={bundleCategory}
+                      onChange={(e) => setBundleCategory(e.target.value)}
+                    >
+                    {/* List of options fo the user to choose from the menu */}
+                    <option value="">Select Category</option>
+                    <option value="baked_goods">Baked Goods</option>
+                    <option value="fruit">Fruit</option>
+                    <option value="vegetables">Vegetables</option>
+                    <option value="meat">Meat</option>
+                    <option value="seafood">Seafood</option>
+                    <option value="SNACKS">Snacks</option>
+                    <option value="dairy">Dairy</option>
+                    <option value="drinks">Drinks</option>
+                    </select>
+                  </div>
+
+
+                  <div>
+                <div className="row">
+                  {/* Outputs an input box with a label asking for bundle allergen inputs */}
+                  <label htmlFor="allergens">Enter Bundle Allergens : </label>
+                  <input
+                    id="allergens"
+                    type="text"
+                    value={bundleAllergens}
+                    onChange={(e) => setBundleAllergens(e.target.value)}
+                  />
+                  </div>
+                </div>
+
+
+                  <div className="row">
+                  {/* Makes an input box with a label to input the number of bundles to sell */}
+                  <label htmlFor="numAvailable">Enter Number Available : </label>
+                  <input
+                    id="numAvailable"
+                    type="number"
+                    value={numberAvailable}
+                    onChange={(e) => setNumberAvailable(e.target.value)}
+                  />
+              </div>
+
+                  <div className="row">
+                  {/* An input box with a label to input the bundle price */}
+                  <label htmlFor="price">Enter Bundle Price : </label>
+                  <input
+                    id="price"
+                    type="number"
+                    value={bundlePrice}
+                    onChange={(e) => setBundlePrice(e.target.value)}
+                  />
+                </div>
+
+                  <div className="row">
+                  {/* Outputs an input box with a label asking for bundle weight */}
+                  <label htmlFor="weight">Enter Bundle Weight : </label>
+                  <input
+                    id="weight"
+                    type="number"
+                    value={bundleWeight}
+                    onChange={(e) => setBundleWeight(e.target.value)}
+                  />
+                </div>
+
+                  <div className="row">
+                  {/* Outputs a drop down menu for the user to click a bundle collection time */}
+                  <label htmlFor="collectionTime">Collection Time: </label>
+                  {/* Divides the start and end time before saving them seperately */}
+                  <select
+                    id="collectionTime"
+                    onChange={(e) => {
+                      const[start,end] = e.target.value.split(" - ");
+                      setStartTime(start);
+                      setEndTime(end);
+                    }}
+                  >
+                    {slots.map((slot,idx) =>(
+                      <option key={idx} value={slot}>
+                        {slot}
+                      </option>
+                    ))}
+                  </select>
+                    {/* Button to add bundles when clicked and forecast the data */}
+                </div>
+                <button className="boxButton" onClick={() => completeUpdateBundle(bundle.posting_id)}>Confirm Update Bundle</button>
+                <button className="boxButton" onClick={() => setUpdateBundle(false)}>Cancel Update</button>
+                </>
+                
+                  
+                
+              )}
+              
+              
+
+              
+
+                
+
+              </div>
+          
             ))}
           </div>
           {noBundles && (
