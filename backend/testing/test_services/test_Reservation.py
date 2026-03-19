53c207e6f548d30e451891e19e49e1ec4f2e1853
FinleyNew <fjfn201@exeter.ac.uk>
2026-03-19 16:36:57 +0000

    Actually fixed the tests

--- backend/testing/test_services/test_Reservation.py
+++ backend/testing/test_services/test_Reservation.py
@@ -1,7 +1,7 @@
 from app.services import reservation as reservation_service
 from app.crud import record as record_crud
 import pytest
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, patch
 from  app.models.enums import ReservationStatus
 
 #Post and reservation class to mimic the real ones and be stored in memory for testing purposes.
@@ -11,8 +11,8 @@ class SimplePosting:
         self.owner_id = owner_id
 
 class SimpleReservation:
-    def __init__(self, user_id, status, claim_code, posting):
-        self.id = user_id
+    def __init__(self, id, status, claim_code, posting):
+        self.user_id = id
         self.status = status
         self.claim_code = claim_code
         self.posting = posting
@@ -21,7 +21,7 @@ class SimpleReservation:
 def test_collection_with_valid_code_success(mock_db):
     post = SimplePosting(id=201, owner_id=2)
     res = SimpleReservation(
-        user_id=101, 
+        id=101, 
         status="PENDING", 
         claim_code="ABC123",
         posting=post
@@ -39,17 +39,17 @@ def test_collection_with_valid_code_success(mock_db):
     mock_db.commit.return_value = None
     mock_db.refresh.return_value = None
 
-    
-    reservation_service.collect_by_code(
-        claim_code="ABC123",
-        
-        db=mock_db
-    )
+    with patch("app.services.reservation.check_at_collection") as mock_check:
+        reservation_service.collect_by_code(
+            claim_code="ABC123",
+            
+            db=mock_db
+        )
 
     
     assert res.status == ReservationStatus.COLLECTED 
     mock_db.commit.assert_called_once() 
-    
+    mock_check.assert_called_once_with(consumer_id=res.user_id, db=mock_db)
 
 
 
@@ -58,7 +58,7 @@ def test_collection_with_valid_code_success(mock_db):
 def test_collection_with_invalid_code(mock_db):
     post = SimplePosting(id=201, owner_id=2)
     res = SimpleReservation(
-        user_id=101, 
+        id=101, 
         status="PENDING", 
         claim_code="ABC123",
         posting=post
