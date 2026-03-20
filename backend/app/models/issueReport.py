526f818f39699e1b3dffae3dd2242b1fe2d86d38
JamesHickson <jmh280@exeter.ac.uk>
2026-03-20 18:44:01 +0000

    Remove type field from model

--- backend/app/models/issueReport.py
+++ backend/app/models/issueReport.py
@@ -1,6 +1,6 @@
 from typing import Optional, List, TYPE_CHECKING
 from sqlmodel import Field, SQLModel, Relationship
-from .enums import ReportType, ReportStatus
+from .enums import ReportStatus
 
 if TYPE_CHECKING:
     from .consumer import Consumer
@@ -10,7 +10,6 @@ class IssueReport(SQLModel, table=True):
     issue_id: Optional[int] = Field(default=None, primary_key=True, index=True)
     posting_id: Optional[int] = Field(default=None, foreign_key="bundleposting.posting_id", index=True)
     user_id: Optional[int] = Field(default=None, foreign_key="consumer.user_id", index=True)
-    type: ReportType
     description: str
     status: ReportStatus = Field(default=ReportStatus.AWAITING_RESPONSE)
     seller_response: str | None = Field(default=None)
