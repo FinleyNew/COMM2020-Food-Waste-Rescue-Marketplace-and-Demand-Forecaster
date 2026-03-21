b2081e27ded796694d0ebe2dd72ab652c8493fe1
harrylewis200 <hl870@exeter.ac.uk>
2026-03-21 12:09:44 +0000

    added default category

--- frontend/src/sellerPages/AddBundles.jsx
+++ frontend/src/sellerPages/AddBundles.jsx
@@ -30,7 +30,7 @@ function AddBundles() {
   
   
   const [bundleAllergens,setBundleAllergens] = useState("");
-  const [bundleCategory, setBundleCategory] = useState("");
+  const [bundleCategory, setBundleCategory] = useState("Baked Goods");
  
   const [bundlePrice,setBundlePrice] = useState("");
   const [numberAvailable, setNumberAvailable] = useState("");
