b808a4aeeda5ff24f56f4b1a0be3f5a143e90e76
TomLambe23 <tl674@exeter.ac.uk>
2026-03-22 16:40:50 +0000

    Add Bundle Formatted

--- frontend/src/sellerPages/AddBundles.jsx
+++ frontend/src/sellerPages/AddBundles.jsx
@@ -259,10 +259,12 @@ function AddBundles() {
               </div>
             </div>
           </div>
-          <div>
-            <h1>Forecasting</h1>
-            <h1>{data.predicted_reservations}</h1>
-            <h1>{data.predicted_no_show_prob}</h1>
+          <div className="mainBox">
+            <div className="forecastEntry">
+              <h1>Forecasting</h1>
+              <h4>Predicted Reservations: {data.predicted_reservations}</h4>
+              <h4>Predicted No Show Probability: {data.predicted_no_show_prob}</h4>
+            </div>
           </div>
       </div>
     </>
