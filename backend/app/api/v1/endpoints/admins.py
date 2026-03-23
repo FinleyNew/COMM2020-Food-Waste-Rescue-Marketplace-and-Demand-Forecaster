9fb7445eaa3ad4355edbf54de778ed53ed9566a7
FinleyNew <fjfn201@exeter.ac.uk>
2026-03-23 21:41:36 +0000

    Added comment to admins

--- backend/app/api/v1/endpoints/admins.py
+++ backend/app/api/v1/endpoints/admins.py
@@ -10,6 +10,7 @@ from app.services import user as user_service
 
 router = APIRouter()
 
+# Endpoint for getting latest test results
 @router.get("/tests")
 def get_test_results(admin: AdminDep):
     if not os.path.exists("test_results.json"):
